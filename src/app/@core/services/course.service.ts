53b28d982c20727448d51e79846fb18f67a14109
Lusian Manolov <lusianmanolov@outlook.com>
2019-06-09 17:32:39 +0300

    Configured environment variables

--- src/app/@core/services/course.service.ts
+++ src/app/@core/services/course.service.ts
@@ -2,6 +2,7 @@ import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
 import { Course } from 'src/app/@modules/course/models/course.model';
+import { environment } from '../../../environments/environment';
 
 @Injectable({
     providedIn: 'root'
@@ -11,25 +12,25 @@ export class CourseService {
     constructor(private http: HttpClient) {}
 
     getAllCourses(): Observable<Course[]> {
-      return this.http.get<Course[]>('http://localhost:3000/courses');
+      return this.http.get<Course[]>(environment.apiUrl + 'courses');
     }
 
     getCourseById(id: string): Observable<Course> {
-      return this.http.get<Course>(`http://localhost:3000/courses/${id}`);
+      return this.http.get<Course>(`${environment.apiUrl}courses/${id}`);
     }
 
     addNewCourse(course: Course): Observable<any> {
       if (course.id) {
-        return this.http.put(`http://localhost:3000/courses/${course.id}`, course);
+        return this.http.put(`${environment.apiUrl}courses/${course.id}`, course);
       }
-      return this.http.post('http://localhost:3000/courses', course);
+      return this.http.post(environment.apiUrl + 'courses', course);
     }
 
     deleteCourse(id: string): Observable<any> {
-      return this.http.delete('http://localhost:3000/courses/' + id);
+      return this.http.delete(environment.apiUrl + 'courses/' + id);
     }
 
     joinCourse(course: Course): Observable<any> {
-      return this.http.put(`http://localhost:3000/courses/${course.id}`, course);
+      return this.http.put(`${environment.apiUrl}courses/${course.id}`, course);
     }
 }
--- src/app/@core/services/user.service.ts
+++ src/app/@core/services/user.service.ts
@@ -2,6 +2,7 @@ import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
 import { User } from 'src/app/@shared/models/user/user.model';
+import { environment } from 'src/environments/environment';
 
 @Injectable({
     providedIn: 'root'
@@ -11,21 +12,21 @@ export class UserService {
     constructor(private http: HttpClient) { }
 
     getAllUsers(): Observable<User[]> {
-        return this.http.get<User[]>('http://localhost:3000/users');
+        return this.http.get<User[]>(environment.apiUrl + 'users');
     }
 
     deleteUser(id: number): Observable<any> {
-        return this.http.delete('http://localhost:3000/users/' + id);
+        return this.http.delete(environment.apiUrl + 'users/' + id);
     }
 
     addNewUser(user: User): Observable<any> {
         if (user.id) {
-            return this.http.put(`http://localhost:3000/users/${user.id}`, user);
+            return this.http.put(`${environment.apiUrl}users/${user.id}`, user);
         }
-        return this.http.post('http://localhost:3000/users', user);
+        return this.http.post(environment.apiUrl + 'users', user);
     }
 
     getById(id: number): Observable<User> {
-        return this.http.get<User>(`http://localhost:3000/users/${id}`);
+        return this.http.get<User>(`${environment.apiUrl}users/${id}`);
     }
 }
