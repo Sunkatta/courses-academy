d63367112d3ab671d3065e7edc89412befd7466f
Lusian Manolov <lusianmanolov@outlook.com>
2019-09-03 15:24:55 +0300

    Added backend service

---
+++ src/app/@core/services/backend.service.ts
@@ -0,0 +1,39 @@
+import { Injectable } from '@angular/core';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { environment } from 'src/environments/environment';
+
+@Injectable({
+    providedIn: 'root'
+})
+export class BackendService {
+
+    constructor(private http: HttpClient) {}
+
+    backendRequest(requestType, requestTarget, requestData?, useUserToken?): Observable<any> {
+        return this.baseBackendRequest(requestType, requestTarget, requestData, useUserToken);
+    }
+
+    private baseBackendRequest(requestType, requestTarget, requestData?, useUserToken?): Observable<any> {
+        if (requestType === 'post') {
+            return this.http.post(
+                environment.apiUrl + requestTarget,
+                requestData,
+                // { headers: this.getContentHeaders(useUserToken) }
+            );
+        } else if (requestType === 'get') {
+            return this.http.get(
+                environment.apiUrl + requestTarget,
+                // { headers: this.getContentHeaders(useUserToken), params: requestData, observe: 'response' }
+            );
+        }
+    }
+
+    // getContentHeaders(useUserToken?: any): HttpHeaders {
+    //     let contentHeaders = new HttpHeaders();
+    //     let authToken = this.authservice.pickAuthenticationToken();
+    //     if (useUserToken) { authToken = this.authservice.userToken; }
+    //     contentHeaders = contentHeaders.set('Authorization', `Bearer ${authToken}`);
+    //     return contentHeaders;
+    // }
+}
