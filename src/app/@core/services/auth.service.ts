51d19ae9fff886f91d7746e5af6412e4d27ff857
Lusian Manolov <lusianmanolov@outlook.com>
2019-09-03 15:23:54 +0300

    Reconfigured Auth Service so that it communicates with IDS4

--- src/app/@core/services/auth.service.ts
+++ src/app/@core/services/auth.service.ts
@@ -1,54 +1,89 @@
 import { Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
-import { Observable } from 'rxjs';
+import { Observable, BehaviorSubject } from 'rxjs';
 import { UserService } from './user.service';
-import { User } from 'src/app/@shared/models/user/user.model';
 import { Router } from '@angular/router';
+import { UserManager, UserManagerSettings, User } from 'oidc-client';
+import { environment } from 'src/environments/environment';
 
 @Injectable({
     providedIn: 'root'
 })
 export class AuthService {
-    constructor(private http: HttpClient, private userService: UserService, private router: Router) {}
+  // Observable navItem source
+  private authNavStatusSource = new BehaviorSubject<boolean>(false);
+  // Observable navItem stream
+  authNavStatus$ = this.authNavStatusSource.asObservable();
 
-    public getLoggedUser(): Observable<User> {
-        if (+sessionStorage.getItem('loggedUserId') === 0) {
-            this.router.navigateByUrl('courses');
-        } else {
-            return new Observable<User>((observer) => {
-                this.userService.getById(+sessionStorage.getItem('loggedUserId'))
-                    .subscribe((response: User) => {
-                        observer.next(response);
-                        observer.complete();
-                    },
-                    () => {
-                        this.logout();
-                    });
-            });
+  private manager = new UserManager(this.getClientSettings());
+  public user: User | null;
+
+  constructor(private userService: UserService, private router: Router) {
+    this.manager.getUser().then(user => {
+      this.user = user;
+      this.authNavStatusSource.next(this.isAuthenticated());
+    });
+  }
+
+  public getLoggedUser(): Observable<User> {
+    if (+sessionStorage.getItem('loggedUserId') === 0) {
+      this.router.navigateByUrl('courses');
+    } else {
+      return new Observable<User>(
+        (observer) => {
+          this.userService.getById(+sessionStorage.getItem('loggedUserId'))
+          .subscribe(
+          (response: any) => {
+            observer.next(response);
+            observer.complete();
+          },
+            () => {
+              this.signout();
+            }
+          );
         }
+      );
     }
+  }
 
-    public login(email: string, password: string): Observable<User> {
-        return new Observable((observer) => {
-            this.userService.getAllUsers().subscribe((allUsers: User[]) => {
-                const user = allUsers.find(u => u.email === email && u.password === password);
-                if (user) {
-                    if (user.isBlocked) {
-                        observer.error('Your account is blocked!');
-                        return;
-                    }
-                    sessionStorage.setItem('loggedUserId', JSON.stringify(user.id));
-                    observer.next(user);
-                    observer.complete();
-                } else {
-                    observer.error('Incorrect username or password!');
-                }
-            });
-        });
-    }
+  login() {
+    return this.manager.signinRedirect();
+  }
 
-    public logout() {
-        sessionStorage.clear();
-        this.router.navigate(['/login']);
-    }
+  signout() {
+    this.manager.signoutRedirect();
+  }
+
+  async completeAuthentication() {
+    new UserManager({response_mode: 'query'}).signinRedirectCallback().then((response) => {
+      this.user = response;
+      this.authNavStatusSource.next(this.isAuthenticated());
+      this.router.navigate(['/courses']);
+    });
+  }
+
+  isAuthenticated(): boolean {
+    return this.user != null && !this.user.expired;
+  }
+
+  get authorizationHeaderValue(): string {
+    return `${this.user.token_type} ${this.user.access_token}`;
+  }
+
+  get name(): string {
+    return this.user != null ? this.user.profile.sub : '';
+  }
+
+  private getClientSettings(): UserManagerSettings {
+    return {
+      authority: environment.ids4Url,
+      client_id: 'ca',
+      client_secret: environment.caSecret,
+      loadUserInfo: true,
+      post_logout_redirect_uri: environment.appUrl + 'courses',
+      redirect_uri: environment.appUrl + 'courses',
+      response_type: 'code',
+      scope: 'openid profile email ca'
+    };
+  }
 }
+
