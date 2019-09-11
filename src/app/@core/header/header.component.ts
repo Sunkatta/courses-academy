77f0c45f1e85fc37905b38a76b95445a300237d4
Lusian Manolov <lusianmanolov@outlook.com>
2019-09-11 16:12:54 +0300

    Changes in header and register component as well as in auth service

--- src/app/@core/header/header.component.ts
+++ src/app/@core/header/header.component.ts
@@ -3,6 +3,7 @@ import { AuthService } from '../services/auth.service';
 import { User } from 'src/app/@shared/models/user/user.model';
 import { UserRole } from 'src/app/@shared/enums/user-role.enum';
 import { Subscription } from 'rxjs';
+import { UserService } from '../services/user.service';
 
 @Component({
   selector: 'app-header',
@@ -13,25 +14,39 @@ export class HeaderComponent implements OnInit, DoCheck {
   isUserLogged = false;
   isAdmin = false;
   user: User;
-
-  name: string;
+  userAcquired = false;
+  name = '';
   isAuthenticated: boolean;
   subscription: Subscription;
 
-  constructor(private authService: AuthService) { }
+  constructor(private authService: AuthService, private userService: UserService) { }
 
   ngOnInit() {
     this.subscription = this.authService.authNavStatus$.subscribe(status => this.isAuthenticated = status);
-    this.name = this.authService.name;
+    this.userAcquired = false;
   }
 
   ngDoCheck() {
-    if (sessionStorage.getItem('loggedUserId') && !this.user) {
-      this.authService.getLoggedUser()
-        .subscribe((response: any) => {
-          this.user = response;
-          this.isAdmin = this.user.role === UserRole.Admin;
-        });
+    // if (sessionStorage.getItem('loggedUserId') && !this.user) {
+    //   this.authService.getLoggedUser()
+    //     .subscribe((response: any) => {
+    //       this.user = response;
+    //       this.isAdmin = this.user.role === UserRole.Admin;
+    //     });
+    // }
+
+    if (sessionStorage.length === 0) {
+      return;
+    }
+
+    if (this.userAcquired === false && this.authService.name !== '') {
+      this.userService.getById(this.authService.name)
+      .subscribe(
+        response => {
+          this.name = response.body.firstName;
+          this.userAcquired = true;
+        }
+      );
     }
 
     if (!sessionStorage.getItem('loggedUserId')) {
@@ -39,7 +54,7 @@ export class HeaderComponent implements OnInit, DoCheck {
       this.isAdmin = false;
     }
 
-    this.name = this.authService.name;
+    // this.name = this.authService.name;
   }
 
   login() {
