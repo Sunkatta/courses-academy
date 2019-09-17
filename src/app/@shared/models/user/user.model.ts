c045a127a1ccf064b83e2e93a5109957dc8206fb
Lusian Manolov <lusianmanolov@outlook.com>
2019-09-17 17:07:17 +0300

    Extended User model

--- src/app/@shared/models/user/user.model.ts
+++ src/app/@shared/models/user/user.model.ts
@@ -1,8 +1,9 @@
 import { UserRole } from '../../enums/user-role.enum';
 
 export class User {
-    id: number;
-    name: string;
+    id: string;
+    firstName: string;
+    lastName: string;
     email: string;
     password: string;
     role: UserRole;
