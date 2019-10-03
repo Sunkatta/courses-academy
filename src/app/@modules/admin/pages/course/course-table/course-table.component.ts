757a7d8763060611a368602762d3f7aee7cfb99f
Lusian Manolov <lusianmanolov@outlook.com>
2019-10-03 15:53:20 +0300

    Added course table in Course-Table component

--- src/app/@modules/admin/pages/course/course-table/course-table.component.ts
+++ src/app/@modules/admin/pages/course/course-table/course-table.component.ts
@@ -1,7 +1,23 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { CourseService } from 'src/app/@core/services/course.service';
+import { Course } from 'src/app/@modules/course/models/course.model';
 
 @Component({
     selector: 'app-course-table',
     templateUrl: './course-table.component.html'
 })
-export class CourseTableComponent { }
+export class CourseTableComponent implements OnInit {
+    courses: Course[];
+
+    constructor(private courseService: CourseService) { }
+
+    ngOnInit() {
+        this.courseService.getRawCourses()
+        .subscribe(
+            response => {
+                this.courses = response.body;
+                console.log(this.courses);
+            }
+        );
+    }
+}
