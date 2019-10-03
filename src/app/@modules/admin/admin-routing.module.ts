17734c0fc924fc5ac114d24148481fc87e737b94
Lusian Manolov <lusianmanolov@outlook.com>
2019-10-03 15:22:42 +0300

    Configured routes for user and course tables

--- src/app/@modules/admin/admin-routing.module.ts
+++ src/app/@modules/admin/admin-routing.module.ts
@@ -1,12 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { AdminComponent } from './admin.component';
+import { UserTableComponent } from './pages/user/user-table/user-table.component';
+import { CourseTableComponent } from './pages/course/course-table/course-table.component';
 
 const routes: Routes = [
     {
         path: '',
+        component: AdminComponent,
         children: [
-            { path: '', component: AdminComponent}
+            { path: 'users', component: UserTableComponent },
+            { path: 'courses', component: CourseTableComponent }
         ]
     }
 ];
