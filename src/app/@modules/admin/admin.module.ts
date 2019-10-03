f6207ba4c6a8ceceb228bf83d1c3d8d3ca013cb6
Lusian Manolov <lusianmanolov@outlook.com>
2019-10-03 14:47:17 +0300

    Changes in Admin Module and Admin Component HTML

--- src/app/@modules/admin/admin.module.ts
+++ src/app/@modules/admin/admin.module.ts
@@ -1,12 +1,18 @@
 import { NgModule, NO_ERRORS_SCHEMA } from '@angular/core';
 import { AdminRoutingModule } from './admin-routing.module';
 import { AdminComponent } from './admin.component';
+import { UserListComponent } from './pages/user-list/user-list.component';
+import { UserItemComponent } from './pages/user-list/user-item/user-item.component';
+import { CommonModule } from '@angular/common';
 
 @NgModule({
     declarations: [
-        AdminComponent
+        AdminComponent,
+        UserListComponent,
+        UserItemComponent
     ],
     imports: [
+        CommonModule,
         AdminRoutingModule
     ],
     schemas: [ NO_ERRORS_SCHEMA ]
