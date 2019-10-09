5a4b561ad3018b1b2fe42cd3898d88b506fd7c07
Lusian Manolov <lusianmanolov@outlook.com>
2019-10-09 15:31:55 +0300

    Improved course deletion flow

--- src/app/@modules/admin/pages/course/course-table/course-table.component.ts
+++ src/app/@modules/admin/pages/course/course-table/course-table.component.ts
@@ -10,8 +10,11 @@ import { Router, ActivatedRoute } from '@angular/router';
 })
 export class CourseTableComponent implements OnInit {
     courses: Course[];
-    successMessage = false;
+    showAlert = false;
     message: string;
+    isForDeletion: boolean;
+    isSuccessful: boolean;
+    courseForDeletion: Course;
 
     constructor(private courseService: CourseService,
                 private router: Router,
@@ -20,7 +23,7 @@ export class CourseTableComponent implements OnInit {
 
     ngOnInit() {
         if (this.route.snapshot.queryParams.title) {
-            this.successMessage = true;
+            this.showAlert = true;
             this.message = 'Course \'' + this.route.snapshot.queryParams.title + '\' created successfuly!';
         }
 
@@ -36,10 +39,36 @@ export class CourseTableComponent implements OnInit {
         this.router.navigateByUrl('admin/courses/add');
     }
 
-    updateCourseTable(data: any) {
-        window.scroll(0, 0);
-        this.courses = data.courses;
-        this.message = data.message;
-        this.successMessage = true;
+    onDeleteClicked(course: Course) {
+        this.isForDeletion = true;
+        this.courseForDeletion = course;
+        this. message =
+            'You are about to delete \'' +
+            this.courseForDeletion.title +
+            '\'!<br><b>This action cannot be undone!</b><br>Are you sure you want to proceed?';
+        this.showAlert = true;
+    }
+
+    deleteCourse() {
+        this.courseService.deleteCourse({
+            CourseId: this.courseForDeletion.id
+        })
+        .subscribe(
+            response => {
+                this.courses = response,
+                this.message = 'Course \'' + this.courseForDeletion.title + '\' successfully deleted!';
+                this.isForDeletion = false;
+                this.isSuccessful = true;
+            }
+        );
+    }
+
+    onCourseEdit(courseId: string) {
+        this.router.navigate(['admin/courses/edit', courseId]);
+    }
+
+    closeAlert() {
+        this.showAlert = !this.showAlert;
+        this.router.navigateByUrl('admin/courses');
     }
 }
